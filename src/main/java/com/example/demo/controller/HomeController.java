0798c61e3208e0434f52f8fab7fba6a1cfcc3a52
Phạm Hà Anh <89311003+D-RubyIV@users.noreply.github.com>
2023-09-24 22:47:44 +0700

    200

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,15 +1,29 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.dto.auth.LoginAuthDto;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName(); // Lấy tên người dùng đã đăng nhập
+        model.addAttribute("username", username);
+        System.out.println(username);
         return "home";
     }
     @GetMapping("/base")
@@ -17,7 +31,9 @@ public class HomeController {
         return "base";
     }
     @GetMapping("/login")
-    public String login(){
+    public String getLogin(){
         return "login";
     }
+
+
 }
